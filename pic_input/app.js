cf1a6369d0ca75f270bb53b43139181e4c2c1bd1
Vsevolod <vsevolodehe@mail.ru>
2022-03-16 13:47:29 +0500

    work in progress

--- pic_input/app.js
+++ pic_input/app.js
@@ -3,13 +3,28 @@ console.log('app.js is connected')
 
 const loadPicButton = document.querySelector('.btn')
 const input = document.querySelector('#input_pic')
+const wrapper = document.querySelector('.wrapper')
 
 const activateLoader = () => input.click()
 
 
+
 function picHandler(event){
-    const files = event.target.files
-    console.log(files)
+    const files = Array.from(event.target.files)
+    
+    files.forEach(file => {
+        const reader =  new FileReader()
+        
+        reader.onload = event => {
+            const image = event.target.result
+            wrapper.insertAdjacentHTML('afterbegin', `<img src = "${image}" class='image_prevew'></img>`)
+
+        }
+        
+        
+        reader.readAsDataURL(file)
+
+    })
     
 
 }
