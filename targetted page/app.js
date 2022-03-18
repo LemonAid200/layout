58f0be4491f7c252cf7c35e8e50fcf83521d4e33
Vsevolod <vsevolodehe@mail.ru>
2022-03-18 19:50:07 +0500

    adding photos is working kinda fine

--- targetted page/app.js
+++ targetted page/app.js
@@ -1,33 +1,41 @@
 console.log('app js has been connected')
 
 
-
 const big_pic_loader = document.querySelector('.pic_load_big')
 const small_pic_loader = document.querySelector('.pic_load_small')
 const big_input = document.querySelector('.big_pic_input')
 const small_input = document.querySelector('.small_pic_input')
 const big_photos_wrapper = document.querySelector('.big_photos_wrapper')
-let big_pics_array_length = 0
+const small_photos_wrapper = document.querySelector('.small_photos_wrapper')
 
 
-
+big_input.addEventListener('change', picHandler)
 big_photos_wrapper.addEventListener('click', removePicture)
+small_input.addEventListener('change', picHandlerSmall)
+small_photos_wrapper.addEventListener('click', removePicture)
+
+
+let big_pics_array_length = 0
+let small_pics_array_length = 0
+
 
 function removePicture(event) {
-    if (!event.target.dataset.name) {
-        return
+    if (!event.target.dataset.name) {return}
+
+    if (event.target.classList[1] == 'big_closing') {
+        big_pics_array_length -= 1
+        const removablePicture = document.querySelector(`[data-name='${event.target.dataset.name}+big']`)
+        removablePicture.remove()
     }
 
-    const removablePicture = document.querySelector(`[data-name='${event.target.dataset.name}']`)
-    removablePicture.remove()
-
-
-    big_pics_array_length-=1
-    console.log(big_pics_array_length)
-
+    if (event.target.classList[1] == 'small_closing') {
+        small_pics_array_length -= 1
+        console.log(small_pics_array_length)
+        const removablePicture = document.querySelector(`[data-name='${event.target.dataset.name}+small']`)
+        removablePicture.remove()
+    }
 
     giveNumbers()
-
 }
 
 
@@ -44,20 +52,14 @@ small_pic_loader.addEventListener('click', (event) =>
 function picHandler(event) {
     file_storage = Array.from(event.target.files)
 
-
-
-
-    file_storage.forEach(file => {
+    file_storage.forEach(file => {       
         const reader = new FileReader()
-
         reader.onload = event => {
-            if (big_pics_array_length == 5){
-                return
-            }
+            if (big_pics_array_length == 5) {return}
             const image = event.target.result
             big_photos_wrapper.insertAdjacentHTML('afterbegin',
-                ` <div style="position: relative; margin-right: 20px; margin-bottom: 20px;" data-name="${file.name}" class = "big_pic_container">
-                    <div class="closeV" data-name="${file.name}"></div>
+                ` <div style="position: relative; margin-right: 20px; margin-bottom: 20px;" data-name="${file.name}+big" class = "big_pic_container">
+                    <div class="closeV big_closing" data-name="${file.name}"></div>
                     <div class="big_preview_box">
                         <img src="${image}" class="big_pic_preview" alt="">
                     </div>
@@ -65,13 +67,38 @@ function picHandler(event) {
                 </div>`
             )
             giveNumbers()
-            big_pics_array_length+=1
-            console.log(big_pics_array_length)
+            big_pics_array_length += 1
         }
         reader.readAsDataURL(file)
     })
 }
 
+function picHandlerSmall(event) {
+    file_storage = Array.from(event.target.files)
+    file_storage.forEach(file => {
+        const reader = new FileReader()
+
+        reader.onload = event => {
+            if (small_pics_array_length == 5) {
+                return
+            }
+            const image = event.target.result
+            small_photos_wrapper.insertAdjacentHTML('afterbegin',
+                ` <div style="position: relative; margin-right: 20px; margin-bottom: 20px;" data-name="${file.name}+small" class = "big_pic_container">
+                    <div class="closeV small_closing" data-name="${file.name}"></div>
+                    <div class="small_preview_box">
+                        <img src="${image}" class="small_pic_preview" alt="">
+                    </div>
+                    <div class="pic_number small_pic_number"></div>
+                </div>`
+            )
+            giveNumbers()
+            small_pics_array_length += 1
+            console.log(small_pics_array_length)
+        }
+        reader.readAsDataURL(file)
+    })
+}
 
 
 function giveNumbers() {
@@ -83,9 +110,17 @@ function giveNumbers() {
         number.innerHTML = index
 
     })
+
+    let arrayOfsmallPicNumbers = document.querySelectorAll('.small_pic_number')
+    let indexS = 0
+    arrayOfsmallPicNumbers = Array.from(arrayOfsmallPicNumbers)
+    arrayOfsmallPicNumbers.forEach(number => {
+        indexS += 1
+        number.innerHTML = indexS
+
+    })
 }
 
 
 
 
-big_input.addEventListener('change', picHandler)
